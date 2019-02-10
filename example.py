bfc9ad64549a6336dc831034a8b8dceed2c8281d
Jeremy Lim <git@jerl.im>
2019-02-11 02:36:13 +0800

    Undo previous commit

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 from pypokerengine.api.game import setup_config, start_poker
 from randomplayer import RandomPlayer
 from raise_player import RaisedPlayer
-from fishplayer import FishPlayer
 
 #TODO:config the config as our wish
 config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
