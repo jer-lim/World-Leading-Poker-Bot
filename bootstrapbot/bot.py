43c38b263c8f1e4f91e9ad86650dc4d087e2e2d7
Jeremy Lim <git@jerl.im>
2019-02-13 17:50:08 +0800

    Add preflop odds to hand scorer

--- bootstrapbot/bot.py
+++ bootstrapbot/bot.py
@@ -10,9 +10,12 @@ class BootStrapBot(BasePokerPlayer):
 
   def declare_action(self, valid_actions, hole_card, round_state):
     pp = pprint.PrettyPrinter(indent=2)
-    self.hand_scorer.score_hole_cards(hole_card)
+    score = self.hand_scorer.score_hole_cards(hole_card)
 
-    return "call"
+    if (score >= 0):
+      return self.__raise_or_call(valid_actions)
+    else:
+      return "fold"
     
 
   def receive_game_start_message(self, game_info):
@@ -29,6 +32,12 @@ class BootStrapBot(BasePokerPlayer):
 
   def receive_round_result_message(self, winners, hand_info, round_state):
     pass
+
+  def __raise_or_call(self, valid_actions):
+    if {"action": "raise"} in valid_actions:
+      return "raise"
+    else:
+      return "call"
 
 def setup_ai():
   return BootStrapBot()
