202ddc3b36d056b4fdd317403cd75ef0dff9316f
pengnam <seanngpengnam@gmail.com>
2019-02-17 10:22:18 +0800

    Add advesarial search

---
+++ adversarial_search.py
@@ -0,0 +1,15 @@
+"""
+This implementation of advesarial search ignores the size of the pot.
+It only models:
+1) Given the state of the community cards, current cards on hand
+2) three possible transition states of each player
+3)
+
+This simple implementation makes a few assumptions:
+1) Currently at the start of the showdown phase.
+2)
+
+"""
+
+class AdversarialSeach:
+    def __init__(self):
