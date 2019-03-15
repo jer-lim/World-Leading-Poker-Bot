65b7fefeaabfc40464b29748d378fd7074a55f95
pengnam <seanngpengnam@gmail.com>
2019-03-15 09:48:50 +0800

    Apply lint

--- adversarial_search.py
+++ adversarial_search.py
@@ -12,18 +12,19 @@ Look at expected value of decision node:
 win_prob * self.pot + (1-win_prob) * (-1) * self.pot
 """
 
-
-
 from pypokerengine.engine.hand_evaluator import HandEvaluator
 from collections import defaultdict
 indent = 0
+
+
 def indent_print(x):
     print("   " * indent + str(x))
 
+
 from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate, _pick_unused_card, gen_deck
 
-
 RAISE_TURN_THRESHOLD = 2
+
 
 class AdversarialSeach:
     def __init__(self, hole_cards, community_cards, pot):
@@ -32,11 +33,18 @@ class AdversarialSeach:
         self.pot = pot
 
     def decide(self, actions):
-        node = DecisionNode(0,self.hole_cards, self.community_cards, self.pot)
+        node = DecisionNode(0, self.hole_cards, self.community_cards, self.pot)
         return node.getBestAction(actions)
 
+
 class DecisionNode:
-    def __init__(self, turn, hole_cards, community_cards, pot, raise_turn = 0, called_or_raised=False):
+    def __init__(self,
+                 turn,
+                 hole_cards,
+                 community_cards,
+                 pot,
+                 raise_turn=0,
+                 called_or_raised=False):
         """
         Params
         ------
@@ -55,16 +63,18 @@ class DecisionNode:
         self.raise_turn = raise_turn
         self.called_or_raised = called_or_raised
         self.win_prob = estimate_hole_card_win_rate(
-                nb_simulation=10,
-                nb_player=2,
-                hole_card=self.hole_cards,
-                community_card=self.community_cards
-                )
-
+            nb_simulation=10,
+            nb_player=2,
+            hole_card=self.hole_cards,
+            community_card=self.community_cards)
 
     def getBestAction(self, actions):
-        action_map = {"raise": self.raise_stakes, "fold":self.fold, "call":self.call}
-        action_funcs = [(action,action_map.get(action)) for action in actions]
+        action_map = {
+            "raise": self.raise_stakes,
+            "fold": self.fold,
+            "call": self.call
+        }
+        action_funcs = [(action, action_map.get(action)) for action in actions]
         results = {}
         for label, func in action_funcs:
             node = func()
@@ -81,11 +91,12 @@ class DecisionNode:
     """
     Returns an expected utility value at the current node
     """
+
     def eval(self):
         global indent
         indent += 1
 
-        indent_print("-MY TURN-"  if not (self.turn) else "-OPP TURN-")
+        indent_print("-MY TURN-" if not (self.turn) else "-OPP TURN-")
         #Terminal node
         if self.remaining_cards == 0 and not self.called_or_raised:
             indent -= 1
@@ -107,19 +118,28 @@ class DecisionNode:
         return f(results)
 
     def expected_value(self):
-        return self.win_prob * self.pot + (1-self.win_prob) * (-1) * self.pot
+        return self.win_prob * self.pot + (1 - self.win_prob) * (-1) * self.pot
+
     """
     Generates next node with raised stakes
     """
+
     def raise_stakes(self):
         if self.raise_turn >= RAISE_TURN_THRESHOLD:
             return None
         indent_print(str(self.turn) + ": EXPLORE RAISED")
-        return DecisionNode(self.opponent, self.hole_cards, self.community_cards, self.pot + 10, self.raise_turn + 1, called_or_raised=True)
+        return DecisionNode(
+            self.opponent,
+            self.hole_cards,
+            self.community_cards,
+            self.pot + 10,
+            self.raise_turn + 1,
+            called_or_raised=True)
 
     """
     Generates a next node that describes terminated value
     """
+
     def fold(self):
         indent_print(str(self.turn) + ": EXPLORE FOLDED")
         return FoldedNode(self.opponent, self.pot)
@@ -127,34 +147,47 @@ class DecisionNode:
     """
     Generates the next node, allows it to go to chance phase
     """
+
     def call(self):
         indent_print(str(self.turn) + ": EXPLORE CALL")
         if self.called_or_raised:
             if self.remaining_cards == 0:
                 return EndingNode(self.expected_value())
             else:
-                return ChanceNode(self.hole_cards, self.community_cards, self.pot)
-        return DecisionNode(self.opponent, self.hole_cards, self.community_cards, self.pot, called_or_raised=True)
+                return ChanceNode(self.hole_cards, self.community_cards,
+                                  self.pot)
+        return DecisionNode(
+            self.opponent,
+            self.hole_cards,
+            self.community_cards,
+            self.pot,
+            called_or_raised=True)
+
 
 class EndingNode:
     def __init__(self, val):
         self.val = val
+
     def eval(self):
         return self.val
+
 
 class FoldedNode:
     def __init__(self, winner, pot):
         self.winner = winner
         self.pot = pot
+
     def eval(self):
         indent_print("A player folded. Winner: " + str(self.winner))
-        return (-1) *self.pot if self.winner else self.pot
+        return (-1) * self.pot if self.winner else self.pot
+
 
 class ChanceNode:
     def __init__(self, hole_cards, community_cards, pot):
         self.hole_cards = hole_cards
         self.community_cards = community_cards
         self.pot = pot
+
     def eval(self):
         """
         Generates and checks only 1 card per meaningful outcome. Returns expected utility.
@@ -163,7 +196,8 @@ class ChanceNode:
         ONEPAIR, FLUSH, etc. Only 1 permutation that gives that outcome is checked.
         """
         # TODO: Optimise gen_hand_rank_info as there are currently alot of useless steps
-        remaining_cards = gen_deck(exclude_cards = self.hole_cards + self.community_cards)
+        remaining_cards = gen_deck(
+            exclude_cards=self.hole_cards + self.community_cards)
         n = remaining_cards.size()
         #Store the utility value given for that outcome
         memo = {}
@@ -171,10 +205,15 @@ class ChanceNode:
         count = defaultdict(int)
         while remaining_cards.size() > 0:
             new_card = remaining_cards.draw_card()
-            strength = HandEvaluator.gen_hand_rank_info(self.hole_cards, self.community_cards + [new_card])["hand"]["strength"]
+            strength = HandEvaluator.gen_hand_rank_info(
+                self.hole_cards,
+                self.community_cards + [new_card])["hand"]["strength"]
             if strength not in memo:
-                memo[strength] = DecisionNode(0, self.hole_cards, self.community_cards + [new_card], self.pot).eval()
+                memo[strength] = DecisionNode(
+                    0, self.hole_cards, self.community_cards + [new_card],
+                    self.pot).eval()
             count[strength] += 1
 
         #Return expected value
-        return sum([count[strength] * memo[strength] for strength in memo.keys()])/n
+        return sum(
+            [count[strength] * memo[strength] for strength in memo.keys()]) / n
