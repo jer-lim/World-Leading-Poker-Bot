5891f926b6efc0cbd5fc49afa82aceed3f196b4d
Jeremy Lim <git@jerl.im>
2019-03-18 17:10:58 +0800

    Add possible flush estimate

--- jer_factors.py
+++ jer_factors.py
@@ -10,39 +10,74 @@ class DecomposedCards:
 		self.community_card = community_card
 
 		# Count suits
-		self.numSuit = {}
-		self.suitHigh = {}
-		for suitNum in Card.SUIT_MAP.keys():
-			self.numSuit[suitNum] = 0
-			self.suitHigh[suitNum] = 0
-		for card in hole_card + community_card:
-			self.numSuit[card.suit] += 1
-			if card.rank > self.suitHigh[suitNum]:
-				self.suitHigh[suitNum] = card.rank
+		self.num_suits = {}
+		self.suit_high = {}
+		self.hole_card_num_suits = {}
+		self.hole_card_suit_high = {}
+		self.comm_card_num_suits = {}
+		self.comm_card_suit_high = {}
+		for suit_num in Card.SUIT_MAP.keys():
+			self.num_suits[suit_num] = 0
+			self.suit_high[suit_num] = 0
+			self.hole_card_num_suits[suit_num] = 0
+			self.hole_card_suit_high[suit_num] = 0
+			self.comm_card_num_suits[suit_num] = 0
+			self.comm_card_suit_high[suit_num] = 0
+
+		for card in hole_card:
+			self.num_suits[card.suit] += 1
+			self.hole_card_num_suits[card.suit] += 1
+			if card.rank > self.suit_high[suit_num]:
+				self.suit_high[suit_num] = card.rank
+			if card.rank > self.hole_card_suit_high[suit_num]:
+				self.hole_card_suit_high[suit_num] = card.rank
+
+		for card in community_card:
+			self.num_suits[card.suit] += 1
+			self.comm_card_num_suits[card.suit] += 1
+			if card.rank > self.suit_high[suit_num]:
+				self.suit_high[suit_num] = card.rank
+			if card.rank > self.comm_card_suit_high[suit_num]:
+				self.comm_card_suit_high[suit_num] = card.rank
 
 		# Count ranks
-		self.numRank = {}
+		self.num_rank = {}
 		for rank in range(1, 15):
-			self.numRank[rank] = 0
+			self.num_rank[rank] = 0
 		for card in hole_card + community_card:
-			self.numRank[card.rank] += 1
+			self.num_rank[card.rank] += 1
 			if card.rank == 14:
-				self.numRank[1] += 1
+				self.num_rank[1] += 1
 
+	# Highest rank in a flush
 	def flushStrength(self):
-		for suit in self.numSuit:
-			if self.numSuit[suit] >= 5:
-				return self.suitHigh[suit]
+		for suit in self.num_suits:
+			if self.num_suits[suit] >= 5:
+				return self.suit_high[suit]
 		return 0
 
+	# Quick estimate number of possible flushes
+	def possibleFlushes(self):
+		count = 0
+		for suit in self.comm_card_num_suits:
+			if self.comm_card_num_suits[suit] >= 3:
+				# Estimate number of flushes higher than the one we have, if we have any
+				if self.num_suits[suit] >= 5:
+					count += 13 - self.suit_high[suit]
+				# If we don't have any, count all possible flushes
+				else:
+					count += 13 - self.num_suits[suit]
+		return count
+
+	# Highest rank in a straight
 	def straightStrength(self):
 		# Look for chain of 5
 		chain = 0
-		for rank in self.numRank:
-			if self.numRank[rank] == 0:
+		for rank in self.num_rank:
+			if self.num_rank[rank] == 0:
 				chain = 0
 			else:
-				chain += self.numRank[rank]
+				chain += self.num_rank[rank]
 			if chain == 5:
 				return rank
 		return 0
