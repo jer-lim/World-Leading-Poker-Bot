099cf292779886726efd03f4b6eee00a08ec9c6d
pengnam <seanngpengnam@gmail.com>
2019-04-13 14:28:10 +0800

    Add alternative plot data implementation

--- plot_data.py
+++ plot_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("data.csv")
+data = pd.read_csv("all_data/data_for_alternative_mwu_final.csv")
 if 'Unnamed: 0' in data.columns:
     data.drop('Unnamed: 0', inplace=True, axis=1)
-data.rename({"ME": "mwu", "no_mwu": "no mwu"}, axis=1, inplace=True)
+data.rename({"ME": "Initial MWU", "no_mwu": "No MWU"}, axis=1, inplace=True)
 data = data/100
 ax = data.plot(style=["r-", "b-"])
 ax.set_xlabel("Rounds")
